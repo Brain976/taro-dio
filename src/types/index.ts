b28b023d7f92e1f4c1d9295e7a64932dc70764c3
Ic <827421256@qq.com>
2020-07-28 11:27:24 +0800

    feat: 完善全局的静态检查

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,66 @@
-export interface RequestConfig {
-    suffix?:string,
-    baseURL:string,
-    header:any,
+export interface ResolvedFn<T> {
+  (val: T): T | Promise<T>;
+}
+
+export interface RejectedFn {
+  (error: any): any;
+}
+
+export type Method =
+  | "get"
+  | "GET"
+  | "delete"
+  | "DELETE"
+  | "head"
+  | "HEAD"
+  | "options"
+  | "OPTIONS"
+  | "post"
+  | "POST"
+  | "put"
+  | "PUT"
+  | "patch"
+  | "PATCH"
+  | "connect"
+  | "CONNECT";
+
+export interface DioConfig {
+  url?: string;
+  method?: Method;
+  data?: any;
+  headers?: any;
+  responseType?: XMLHttpRequestResponseType;
+  timeout?: number;
+}
+
+export interface DioResponse<T = any> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: any;
+  config: DioConfig;
+  request: any;
+}
+
+export interface DioPromise<T = any> extends Promise<DioResponse<T>> {}
+
+export interface AxiosError extends Error {
+  config: DioConfig
+  code?: string
+  request?: any
+  response?: DioConfig
+  isAxiosError: boolean
+}
+
+export interface Dio {
+  interceptors: {
+    request: DioInterceptorManager<DioConfig>
+    response: DioInterceptorManager<DioResponse>
+  }
+}
+
+export interface DioInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number;
+
+  eject(id: number): void;
 }
