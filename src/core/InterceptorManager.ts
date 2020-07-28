983737c275c7900558028acb6d3380c506223592
Ic <827421256@qq.com>
2020-07-28 11:25:03 +0800

    feat(core): 完善拦截器的静态检查

--- src/core/InterceptorManager.ts
+++ src/core/InterceptorManager.ts
@@ -2,37 +2,47 @@
  * 注册拦截器
  */
 
-class InterceptorManager {
-  private handlers: [];
+import { ResolvedFn, RejectedFn } from "../types";
+
+interface Interceptor<T> {
+  resolved: ResolvedFn<T>;
+  rejected?: RejectedFn;
+}
+
+class InterceptorManager<T> {
+  private interceptors: Array<Interceptor<T> | null>;
 
   constructor() {
-    this.handlers = [];
+    this.interceptors = [];
   }
 
   /**
    * 添加一个拦截器
    */
-  use(fulfilled, rejected): number {
-    this.handlers.push({ fulfilled, rejected });
-    return this.handlers.length - 1;
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number {
+    this.interceptors.push({
+      resolved,
+      rejected,
+    });
+    return this.interceptors.length - 1;
   }
 
   /**
    * 移除一个拦截器
    */
-  eject(id: number) {
-    if (this.handlers[id]) {
-      this.handlers[id] = null;
+  eject(id: number): void {
+    if (this.interceptors[id]) {
+      this.interceptors[id] = null;
     }
   }
 
   /**
    * 遍历所有已注册的拦截器
    */
-  forEach(fn: (arg0: any) => void) {
-    this.handlers.forEach((h) => {
-      if (h !== null) {
-        fn(h);
+  forEach(fn: (interceptor: Interceptor<T>) => void): void {
+    this.interceptors.forEach((interceptor) => {
+      if (interceptor !== null) {
+        fn(interceptor);
       }
     });
   }
